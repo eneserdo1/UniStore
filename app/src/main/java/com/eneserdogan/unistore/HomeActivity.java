1b72bb552041e205fdbd134566b4cd8564f560cf
eneserdo1 <eneserdogan.66@outlook.com>
2020-03-27 15:56:33 +0300

    fragment eklendi

--- app/src/main/java/com/eneserdogan/unistore/HomeActivity.java
+++ app/src/main/java/com/eneserdogan/unistore/HomeActivity.java
@@ -26,9 +26,9 @@ public class HomeActivity extends AppCompatActivity {
         //AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 //R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                // .build();
-       //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-       // NavigationUI.setupWithNavController(navView, navController);
+        // NavigationUI.setupWithNavController(navView, navController);
         navView.setOnNavigationItemSelectedListener(navListener);
         loadFragment(homeFragment.newInstance("",""));
     }
